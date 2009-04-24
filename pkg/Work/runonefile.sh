6a731f8b9f135e252a5e91f2524a750dde6bf0f2
hcorrada <hcorrada@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-04-24 07:48:22 +0000

    version 0.1-4, fixes bug on matrices with single non-zero entries

--- pkg/Work/runonefile.sh
+++ pkg/Work/runonefile.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+RUNIT_DIR=$PWD
+PKG_PATH=$(dirname ${RUNIT_DIR})
+shopt -s nullglob
+SOURCES=( ${PKG_PATH}/src/*.c )
+SHLIBNAME=$(basename ${SOURCES[0]} .c)
+
 TEST_FILE=$1
 
 if test $# -ne 1 ; then
@@ -12,4 +18,4 @@ R_CODE="${R_CODE};source(\"setuptests.R\")"
 R_CODE="${R_CODE};res <- runTestFile('${TEST_FILE}',rngKind='default',rngNormalKind='default')"
 R_CODE="${R_CODE};printTextProtocol(res)"
 
-echo ${R_CODE} | R --slave
+echo ${R_CODE} | R --slave --args ${SHLIBNAME}
